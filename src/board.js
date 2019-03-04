0e12102128888bc256c12f1fd1e46527240c7021
calebdav <calebdav@gmail.com>
2019-03-04 16:11:47 -0700

    finished

--- src/board.js
+++ src/board.js
@@ -1,14 +1,48 @@
 import React from 'react';
 import './index.css';
 import Square from './square';
+import { calculateWinner, calculateTie } from './utils';
 
 class Board extends React.Component {
+  state = {
+    squares: Array(9).fill(null),
+    xIsNext: true,
+  }
+
+  handleReset = () => {
+    this.setState({
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    })
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({squares, xIsNext: !this.state.xIsNext})
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />;
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    const isTie = calculateTie(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else if (isTie) {
+      status = 'Its a tie';
+    }else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -28,6 +62,9 @@ class Board extends React.Component {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
+        <button onClick={this.handleReset}>
+          Reset
+        </button>
       </div>
     );
   }
